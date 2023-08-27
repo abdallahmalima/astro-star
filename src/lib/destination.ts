1def3cfc65f4fb30fd3b3496a11df7aaa85cc0ee
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-08-27 19:30:54 -0400

    fdfg

---
+++ src/lib/destination.ts
@@ -0,0 +1,124 @@
+
+ const DESTINATIONS=[
+{
+    id:1,
+    slug:"ngorongoro-creater",
+    title:"NGORONGORO CRATER",
+    image:"assets/images/ngorongoro.jpg",
+    Header_images:[
+        {lg:"/assets/images/des-1-lg.jpg",sm:"/assets/images/des-1.jpg"},
+        {lg:"/assets/images/des-2-lg.jpg",sm:"/assets/images/des-2.jpg"},
+        {lg:"/assets/images/des-3-lg.jpg",sm:"/assets/images/des-3.jpg"},
+        {lg:"/assets/images/des-4-lg.jpg",sm:"/assets/images/des-4.jpg"},
+        {lg:"/assets/images/des-5-lg.jpg",sm:"/assets/images/des-5.jpg"},
+    ],
+    p1:[
+        "The jewel in Ngorongoro's crown is a deep, volcanic crater, the largest un flooded and unbroken caldera in the world. About 20kms across, 600 meters deep and 300 sq kms in area, the Ngorongoro Crater is a breathtaking natural wonder.",
+        "The Ngorongoro Crater is one of Africa’s most famous sites and is said to have the highest density of wildlife in Africa. Sometimes described as an ‘eighth wonder of the world’, the Crater has achieved world renown, attracting an ever-increasing number of visitors each year. You are unlikely to escape other vehicles here, but you are guaranteed great wildlife viewing in a genuinely mind-blowing environment. There is nowhere else in Africa quite like Ngorongoro!",
+        "The Ngorongoro Crater is the world’s largest intact volcanic caldera. Forming a spectacular bowl of about 265 square kilometres, with sides up to 600 metres deep; it is home to approximately 30,000 animals at any one time. The Crater rim is over 2,200 metres high and experiences its own climate. From this high vantage point it is possible to make out the tiny shapes of animals making their way around the crater floor far below. Swathes of cloud hang around the rocky rim most days of the year and it’s one of the few places in Tanzania where it can get chilly at night.",
+        "The crater floor consists of a number of different habitats that include grassland, swamps, forests and Lake Makat (Maasai for ‘salt’) - a central soda lake filled by the Munge River. All these various environments attract wildlife to drink, wallow, graze, hide or climb. Although animals are free to move in and out of this contained environment, the rich volcanic soil, lush forests and spring source lakes on the crater floor (combined with fairly steep crater sides) tend to incline both grazers and predators to remain throughout the year."
+    ],
+    p2:[
+        "Ngorongoro Crater is one of the most likely areas in Tanzania to see the endangered Black Rhino, as a small population is thriving in this idyllic and protected environment. It is currently one of the few areas where they continue to breed in the wild. Your chances of encountering leopard here are also good, and fabulous black-maned lions. Many flamingos are also attracted to the soda waters of Lake Magadi.",
+        "Part of the reason behind the Ngorongoro Conservation Area has been to preserve the environment for the Maasai people who were diverted from the Serengeti Plains. Essentially nomadic people, they build temporary villages in circular homesteads called bomas. There are possibilities to visit a couple of these now, which have been opened up for tourists to explore. Here you can see how the huts are built in a strict pattern of order according to the chronological order of the wives, and experience what it must be like to rely on warmth and energy from a fire burning at the heart of a cattle dung dwelling with no chimney. These proud cattle herding people have a great history as warriors, and even though they are no longer allowed to build villages inside, they continue to herd their cattle into the crater to graze and drink, regardless of the predators nearby.",
+    ]
+},
+{
+    id:2,
+    slug:"serengeti-national-park",
+    title:"SERENGETI NATIONAL PARK",
+    image:"assets/images/serengeti.jpg",
+    p1:[
+        "Serengeti National Park is undoubtedly the best-known wildlife sanctuary in the world, unequalled for its natural beauty and scientific value, it has the greatest concentration of plains game in Africa",
+        "The Serengeti National Park in Tanzania was established in 1952. It is home to the greatest wildlife spectacle on earth - the great migration of wildebeest and zebra. The resident population of lion, cheetah, elephant, giraffe, and birds is also impressive. There’s a wide variety of accommodation available, from luxury lodges to mobile camps. The park covers 5,700 sq miles, (14,763 sq km), it’s larger than Connecticut, with at most a couple hundred vehicles driving around.",
+        "The Park can be divided into 3 sections. The popular southern/central part (Seronera Valley), is what the Maasai called the “serengit”, the land of endless plains. It’s classic savannah, dotted with acacias and filled with wildlife. The western corridor is marked by the Grumeti River, and has more forests and dense bush. The north, Lobo area, meets up with Kenya’s Masai Mara Reserve, is the least visited section.",
+        "Two World Heritage Sites and two Biosphere Reserves have been established within the 30,000 km² region. It’s unique ecosystem has inspired writers from Ernest Hemingway to Peter Mattheissen, filmakers like Hugo von Lawick and Alan Root as well as numerous photographers and scientists - many of which have put their works at our disposal to create this website.",
+        "The Serengeti ecosystem is one of the oldest on earth. The essential features of climate, vegetation and fauna have barely changed in the past million years. Early man himself made an appearance in Olduvai Gorge about two million years ago. Some patterns of life, death, adaptation and migration are as old as the hills themselves."
+        
+    ],
+    p2:[
+        "It is the migration for which Serengeti is perhaps most famous. Over a million wildebeest and about 200,000 zebras flow south from the northern hills to the southern plains for",
+        "the short rains every October and November, and then swirl west and north after the long rains in April, May and June. So strong is the ancient instinct to move that no drought, gorge or crocodile infested river can hold them back.",
+        "The Wildebeest travel through a variety of parks, reserves and protected areas and through a variety of habitat. Join us to explore the different forms of vegetation and landscapes of the Serengeti ecosystem and meet some of their most fascinating inhabitants."
+    ]
+},
+{
+    id:3,
+    slug:"mount-kilimanjaro",
+    title:"MOUNT KILIMANJARO",
+    image:"assets/images/kilimanjaro.jpg",
+    p1:[
+        "At 5896m Mt Kilimanjaro, Africa's highest mountain and one of the continent's magnificent sights, It has three main volcanic peaks, Kibo, Mawenzi, and Shira. The name itself \"Kilimanjaro\" is a mystery wreathed in clouds. It might mean Mountain of Light, Mountain of Greatness or Mountain of Caravans.",
+        "Above the gently rolling hills and plateaux of northern Tanzania rises the snowy peak of Mt. Kilimanjaro, it’s slopes and glaciers shimmering above the rising clouds. Kilimanjaro is located near the town of Moshi and is a protected area, carefully regulated for climbers to enjoy without leaving a trace of their presence. The mountain’s ecosystems are as strikingly beautiful as they are varied and diverse. On the lowland slopes, much of the mountain is farmland, with coffee, banana, cassava, and maize crops grown for subsistence and cash sale. A few larger coffee farms still exist on the lower slopes, but much of the area outside the national park has been subdivided into small plots. Once inside the park, thick lowland forest covers the lower altitudes and breaks into alpine meadows once the air begins to thin. Near the peak, the landscape is harsh and barren, with rocks and ice the predominant features above a breathtaking African view.",
+        "Climbing Mt. Kilimanjaro is the highlight of most visitors’ experiences in Tanzania. Few mountains can claim the grandeur, the breathtaking views of Amboseli National Park in Kenya, the Rift Valley, and the Masaai Steppe, that belongs to Kilimanjaro. Hiking on the ‘rooftop of Africa’ — the highest point on the continent at 5896 metres — is the adventure of a lifetime, especially because, if paced well, everyone from seasoned trekkers to first-time enthusiasts can scale the snowy peak. For more information, see the ‘Mountain Climbing‘ section under ‘Things to Do.",
+        "Kilimanjaro. The name itself is a mystery wreathed in clouds. It might mean Mountain of Light, Mountain of Greatness or Mountain of Caravans. Or it might not. The local people, the Wachagga, don’t even have a name for the whole massif, only Kipoo (now known as Kibo) for the familiar snowy peak that stands imperious, overseer of the continent, the summit of Africa.",
+        "Kilimanjaro, by any name, is a metaphor for the compelling beauty of East Africa. When you see it, you understand why. Not only is this the highest peak on the African continent; it is also the tallest free-standing mountain in the world, rising in breathtaking isolation from the surrounding coastal scrubland – elevation around 900 metres – to an imperious 5,895 metres (19,336 feet)."
+    ],
+    p2:[
+        "Kilimanjaro is one of the world’s most accessible high summits, a beacon for visitors from around the world. Most climbers reach the crater rim with little more than a walking stick, proper clothing and determination. And those who reach Uhuru Point, the actual summit, or Gillman’s Point on the lip of the crater, will have earned their climbing certificates. And their memories.",
+        "But there is so much more to Kili than her summit. The ascent of the slopes is a virtual climatic world tour, from the tropics to the Arctic.",
+        "Even before you cross the national park boundary (at the 2,700m contour), the cultivated footslopes give way to lush montane forest, inhabited by elusive elephant, leopard, buffalo, the endangered Abbot’s duiker, and other small antelope and primates. Higher still lies the moorland zone, where a cover of giant heather is studded with otherworldly giant lobelias.",
+        "Above 4,000m, a surreal alpine desert supports little life other than a few hardy mosses and lichen. Then, finally, the last vestigial vegetation gives way to a winter wonderland of ice and snow – and the magnificent beauty of the roof of the continent.",
+    ]
+},
+{
+    id:4,
+    slug:"zanzibar-island",
+    title:"ZANZIBAR ISLAND",
+    image:"assets/images/zanzibar.jpg",
+    p1:[
+        'Known as the Spice Island, the beautiful island of Zanzibar on Africa’s east coast is bursting with culture and history, seemingly at odds with its idyllic geography of white-sand beaches with palms swaying lazily in the sea breeze. Together this makes Zanzibar a fabulous place to explore as well as a dream to relax and unwind.',
+        'Zanzibar is the semi-autonomous part of Tanzania in East Africa. It is composed of the Zanzibar Archipelago in the Indian Ocean, 25–50 kilometres (16–31 mi) off the coast of the mainland, and consists of many small islands and two large ones: Unguja (the main island, referred to informally as Zanzibar) and Pemba. The capital is Zanzibar City, located on the island of Unguja. Its historic centre is Stone Town, which is a World Heritage Site.',
+        'Portuguese invasion and control of the Swahili Coast in the late 16th century ended the golden age of the archipelago, although the Omani Arabs returned to power less than a century later. Today, many of the winding streets and high townhouses of old Stone Town remain unchanged and visitors can walk between the sultan’s palace, the House of Wonders, the Portuguese fort and gardens, the merchants’ houses, and the Turkish baths of the old city. Day-long spice tours to working plantations offer visitors the chance to observe the cultivation of cloves, vanilla, nutmeg, cinnamon, and other spices that have made the island famous.',
+        
+    ],
+    p2:[
+        'Zanzibar’s coastline offers some of the best beaches in the world, but sand and surf vary depending on what side of the island you’re on. On the east coast, waves break over coral reefs and sand bars offshore, and low tide reveals small pools of starfish, small minnows, and anemones. Up north, ocean swimming is much less susceptible to the tides, and smooth beaches and white sand make for dazzling days in the sun.',
+        'The port city of Stone Town dominates the west coast, and although the beaches of Mangapwani, where slave caves are visible at low tide and nearby Bububu are less than half an hour’s drive away, a night or two spent on the east or north cost is well worth the extra hour it takes to drive there. That said, the Chole Island Marine Park just off Stone Town – and nearby Prison, Grave, and Snake Islands – make a refreshing day-trip and a good break from exploring the winding passageways of the old city.',
+        'On the south coast of Zanzibar lies the Menai Bay Conservation Area, a sea turtle protection area for the endangered species that come to breed on the island. Roads to the southeast coast take visitors through the Jozani Forest, home to Zanzibar’s rare Red Colobus monkeys and a number of other primate and small antelope species.',
+    ]
+},
+{
+    id:5,
+    slug:"tarangire-national-park",
+    title:"TARANGIRE NATIONAL PARK",
+    image:"assets/images/tarangire.jpg",
+    p1:[
+        "Tarangire National Park has some of the highest population density of elephants as compared to anywhere in Tanzania, and its sparse vegetation, strewn with baobab and acacia trees, makes it a beautiful and distinctive location to visit.",
+        "Located just a few hours drive from the town of Arusha, Tarangire is a popular stop for people travelling through the northern safari circuit on their way to Ngorongoro and the Serengeti. The park extends into two game controlled areas and the wildlife is allowed to move freely throughout.",
+        "Before the rains, droves of gazelles, wildebeests, zebras, and giraffes migrate to Tarangire National Park’s scrub plains where the last grazing land still remains. Tarangire offers an unparalleled game viewing, and during the dry season elephants abound. Families of the pachyderms play around the ancient trunks of baobab trees and strip acacia bark from the thorn trees for their afternoon meal. Breathtaking views of the Maasai Steppe and the mountains in the south make a stopover at Tarangire a memorable experience.",
+        "Herds of up to 300 elephants scratch the dry river bed for underground streams, while migratory wildebeest, zebra, buffalo, impala, gazelle, hartebeest and eland crowd the shrinking lagoons. It’s the greatest concentration of wildlife outside the Serengeti ecosystem - a smorgasbord for predators – and the one place in Tanzania where dry-country antelope such as the stately fringe-eared oryx and peculiar long-necked gerenuk are regularly observed.",
+        "During the rainy season, the seasonal visitors scatter over a 20,000 sq km (12,500 sq miles) range until they exhaust the green plains and the river calls once more. But Tarangire’s mobs of elephant are easily encountered, wet or dry. The swamps, tinged green year round, are the focus for 550 bird varieties, the most breeding species in one habitat anywhere in the world.",
+        
+    ],
+    p2:[
+       "On drier ground you find the Kori bustard, the heaviest flying bird; the stocking-thighed ostrich, the world’s largest bird; and small parties of ground hornbills blustering like turkeys.",
+       "More ardent bird-lovers might keep an eye open for screeching flocks of the dazzlingly colourful yellow-collared lovebird, and the somewhat drabber rufous-tailed weaver and ashy starling – all endemic to the dry savannah of north-central Tanzania.",
+       "Disused termite mounds are often frequented by colonies of the endearing dwarf mongoose, and pairs of red-and-yellow barbet, which draw attention to themselves by their loud, clockwork-like duetting.",
+       "Tarangire’s pythons climb trees, as do its lions and leopards, lounging in the branches where the fruit of the sausage tree disguises the twitch of a tail."
+    ]
+},
+{
+    id:6,
+    slug:"lake-manyara",
+    title:"LAKE MANYARA",
+    image:"assets/images/manyara.jpg",
+    p1:[
+        "Located beneath the cliffs of the Manyara Escarpment, on the edge of the Rift Valley, Lake Manyara National Park offers varied ecosystems, incredible bird life, and breathtaking views.",
+        "Located on the way to Ngorongoro Crater and the Serengeti, Lake Manyara National Park is worth a stop in its own right. Its ground water forests, bush plains, baobab strewn cliffs, and algae-streaked hot springs offer incredible ecological variety in a small area, rich in wildlife and incredible numbers of birds.",
+        "The alkaline soda of Lake Manyara is home to an incredible array of bird life that thrives on its brackish waters. Pink flamingo stoop and graze by the thousands colourful specks against the grey minerals of the lake shore. Yellow-billed storks swoop and corkscrew on thermal winds rising up from the escarpment, and herons flap their wings against the sun-drenched sky. Even reluctant bird-watchers will find something to watch and marvel at within the national park.",
+        "Lake Manyara’s famous tree-climbing lions are another reason to pay a visit to this park. The only kind of their species in the world, they make the ancient mahogany and elegant acacias their home during the rainy season, and are a well-known but rather rare feature of the northern park. In addition to the lions, the national park is also home to the largest concentration of baboons anywhere in the world — a fact that accounts for interesting game viewing of large families of the primates.",
+    ],
+    p2:[
+        "Stretching for 50km along the base of the rusty-gold 600-metre high Rift Valley escarpment, Lake Manyara is a scenic gem, with a setting extolled by Ernest Hemingway as “the loveliest I had seen in Africa”.",
+        "The compact game-viewing circuit through Manyara offers a virtual microcosm of the Tanzanian safari experience.",
+        "From the entrance gate, the road winds through an expanse of lush jungle-like groundwater forest where hundred-strong baboon troops lounge nonchalantly along the roadside; the blue monkeys scamper nimbly between the ancient mahogany trees; dainty bushbuck tread warily through the shadows, and the outsized forest hornbills honk cacophonously in the high canopy.",
+        "In contrast with the intimacy of the forest, is the grassy floodplain and its expansive views eastward, across the alkaline lake, to the jagged blue volcanic peaks that rise from the endless Maasai Steppes. Large buffalo, wildebeest and zebra herds congregate on these grassy plains, and so do the giraffes – some so dark in coloration that they appear to be black from a distance.",
+        "Inland of the floodplain, a narrow belt of acacia woodland is the favoured haunt of Manyara’s legendary tree-climbing lions and impressively tusked elephants. Squadrons of banded mongoose dart between the acacias, whereas the diminutive Kirk’s dik-dik forages in their shade. Pairs of klipspringer are often seen silhouetted on the rocks above a field of searing hot springs that steams and bubbles adjacent to the lakeshore in the far south of the park.",
+        "Manyara provides the perfect introduction to Tanzania’s birdlife. More than 400 species have been recorded, and even a first-time visitor to Africa might reasonably expect to observe 100 of these in one day. Highlights include thousands of pink-hued flamingos on their perpetual migration, as well as other large water birds such as pelicans, cormorants and storks.",
+    ]
+},
+]
+
+export default DESTINATIONS;
+
